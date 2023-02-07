ca328c619c4cbeb870b23e87bf966129d3b468dd
zwelinux <zwelinux@gmail.com>
2023-02-07 16:11:24 +0630

    first commit

---
+++ config/middlewares.js
@@ -0,0 +1,40 @@
+module.exports = [
+  'strapi::errors',
+
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'zwe-strapi-media.s3.ap-southeast-1.amazonaws.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            'zwe-strapi-media.s3.ap-southeast-1.amazonaws.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  
+  'strapi::security',
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
